53cd73e55421fdbb735d84e0300565cd839aa706
tylerhoward <tylernhoward@gmail.com>
2017-06-28 18:58:43 -0400

    SDL cycle checklist fix

--- content/Software Development Lifecycle/CS0 C++/checklist.js
+++ content/Software Development Lifecycle/CS0 C++/checklist.js
@@ -1,10 +1,13 @@
 //Might not be working yet
-if (document.getElementById("securityChecklist-questions-q1").checked === true &&
-    document.getElementById("securityChecklist-questions-q2").checked === true &&
-    document.getElementById("securityChecklist-questions-q3").checked === true &&
-    document.getElementById("securityChecklist-questions-q4").checked === true &&
-    document.getElementById("securityChecklist-questions-q5").checked === true
-   ) {
-    document.getElementById('securityChecklist-questions-q6').disabled = false;
-    document.getElementById("securityChecklist-questions-q6").checked = true;
-}
+$(document).ready(function() {
+  $("#securityChecklist-questions").change(function(){
+    if (document.getElementById("securityChecklist-questions-q1").checked === true &&
+        document.getElementById("securityChecklist-questions-q2").checked === true &&
+        document.getElementById("securityChecklist-questions-q3").checked === true &&
+        document.getElementById("securityChecklist-questions-q4").checked === true &&
+        document.getElementById("securityChecklist-questions-q5").checked === true
+       ) {
+        document.getElementById("securityChecklist-questions-q6").checked = true;
+    }
+  });
+});
