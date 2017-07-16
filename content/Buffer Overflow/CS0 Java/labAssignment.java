ca2c9d7e17476b28dd9e8a9aebd866ed37dd07d0
Jewel7 <volleyballjewel@gmail.com>
2017-07-16 02:32:01 -0400

    Buffer Overflow CS0 Java
    This module is incomplete.
    
    1. I added a video link instead of having the actual video on the site.
    
    2. The “security checklist” is bugged. Even though you will complete
    each checkbox, it will still keep you on that page and say that your
    answers are incorrect/try again.
    
    3. The “Discussion Questions” tab is incomplete because I couldn’t
    proceed past the “Security Checklist” section. I just assumed it had
    the same discussion questions as CS0 C++.
    
    *also, does the real-world example source link lead to a dead site?

---
+++ content/Buffer Overflow/CS0 Java/labAssignment.java
@@ -0,0 +1,14 @@
+public class Overflow
+{
+  public static void main(String[] args)
+  {
+    int importantData =1;
+    int[]  buffer = new int[10];
+
+    for (int i =0; i < 15; i++)
+      buffer[i] = 7;
+
+    System.out.println("after buffer overflow ");
+    System.out.println("Important data  = "+importantData);
+  }
+}
