75e4d8b55fbd9470eb922768c7ab42e7ec2160e2
Jewel7 <volleyballjewel@gmail.com>
2017-07-16 02:03:24 -0400

    Buffer Overflow CS0 C++
    This module is incomplete:
    
    1. I added a video link instead of having the actual video on the site.
    
    2. I need help with the security checklist part.

--- create.js
+++ create.js
@@ -124,7 +124,7 @@ exports.write_3("Input Validation", "CS1 Java");
 exports.write_3("Input Validation", "CS1 Python");
 exports.write_3("Input Validation", "CS2 C++");
 exports.write_3("Input Validation", "CS2 Java");
-//exports.write_3("Buffer Overflow", "CS0 C++");
+exports.write_3("Buffer Overflow", "CS0 C++");
 //exports.write_3("Buffer Overflow", "CS0 Java");
 //exports.write_3("Buffer Overflow", "CS0 Python");
 //exports.write_3("Buffer Overflow", "CS0 Pseudocode");
