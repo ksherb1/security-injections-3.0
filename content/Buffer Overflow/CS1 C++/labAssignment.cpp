3876a6b3ae6686ffe4dedd8f3eda5937a2bc761a
Jewel7 <volleyballjewel@gmail.com>
2017-07-17 15:16:18 -0400

    Buffer Overflow CS1 C++
    This module is incomplete because:
    
    1. I added a video link instead of having the actual video on the site.
    
    2. I need help with the security checklist part.

---
+++ content/Buffer Overflow/CS1 C++/labAssignment.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  int vals[10];
+  size_t count;
+  size_t which;
+
+  cout << "How many values should be stored in the array? ";
+  cin >> count;
+
+  for (size_t i = 0; i < count; i++) {
+    vals[i] = count - i;
+  }
+
+  cout << "Which value do you wish to retrieve? ";
+  cin >> which;
+
+  cout << "Your value is " << vals[which] << endl;
+
+  return 0;
+}
