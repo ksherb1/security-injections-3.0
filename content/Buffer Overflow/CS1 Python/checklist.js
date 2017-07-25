9a4e47b68d411f0c6d1742c0eceb27823bcb84b9
Jewel7 <volleyballjewel@gmail.com>
2017-07-25 14:03:48 -0400

    Buffer Overflow CS1 Python Security Checklist
    This module is now complete.

--- content/Buffer Overflow/CS1 Python/checklist.js
+++ content/Buffer Overflow/CS1 Python/checklist.js
@@ -51,8 +51,8 @@ $(document).ready(function() {
 						}
 					});
 					var spanLoc;
-					if(next === name+"-var-range"){spanLoc = 11;} //NEED TO BE SET TO THE RIGHT SPAN
-					else if (next === name+'-loop-range'){spanLoc = 6;}
+					if(next === name+"-var-range"){spanLoc = 6;} //NEED TO BE SET TO THE RIGHT SPAN
+					else if (next === name+'-loop-range'){spanLoc = 4;}
 
 					//show Popover over the index span
 					showPopOver(spans[spanLoc]);
